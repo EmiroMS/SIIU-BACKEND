f3b0a5d759d69e6a587c9e30a3737d86627f2c8d
EmiroMS <EmiroMS>
2025-09-04 00:28:43 -0500

    entorno

---
+++ src/main/java/com/siiu/SiiuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiuBackendApplication.class, args);
+	}
+
+}
